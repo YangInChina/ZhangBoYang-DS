fb0aee5fa7c0df0b4a40138c976a85d817a959c8
BoYang_Zhang <91548907+YangInChina@users.noreply.github.com>
2021-10-10 17:42:55 +0800

    Create _Tree_H.c

---
+++ week2/ch01/_Tree_H.c
@@ -0,0 +1,19 @@
+#pragma once
+
+#include<stdlib.h>
+#include<stdio.h>
+
+
+struct TreeNode;
+typedef struct TreeNode* Position;
+typedef struct TreeNode* SearchTree;
+
+typedef int ElementType;
+
+SearchTree MakeEmpty(SearchTree T);
+Position Find(ElementType X, SearchTree T);
+Position FindMin(SearchTree T);
+Position FindMax(SearchTree T);
+SearchTree Insert(ElementType X, SearchTree T);
+SearchTree Delete(ElementType X, SearchTree T);
+ElementType Retrieve(Position P);
