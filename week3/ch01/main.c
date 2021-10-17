4f8467c2d28f874f31da5dbd059cb6f146f971af
BoYang_Zhang <91548907+YangInChina@users.noreply.github.com>
2021-10-17 19:48:39 +0800

    Create main.c

---
+++ week3/ch01/main.c
@@ -0,0 +1,20 @@
+void main() {
+	const char* Key;
+	Key = "Apple";
+	int TableSize = 10007;
+	Hash(Key, TableSize);
+
+    int i;
+    const char* A="Apple";
+
+    HashTable hashTable= InitializeTable(7);
+
+
+    for (i = 0; i < 7; i++)
+    {
+        Insert(&A[i], hashTable);
+    }
+
+    Display(hashTable);
+
+}
