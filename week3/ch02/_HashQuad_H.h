e1dc0797b44e567b508118390abe1e4b8d97b9b7
BoYang_Zhang <91548907+YangInChina@users.noreply.github.com>
2021-10-17 20:58:44 +0800

    Update _HashQuad_H.h

--- week3/ch02/_HashQuad_H.h
+++ week3/ch02/_HashQuad_H.h
@@ -1,19 +1,17 @@
 #pragma once
-#include<stdio.h>
 #include<stdlib.h>
+#include<stdio.h>
 
 typedef unsigned int Index;
+typedef Index Position;
 
-Index Hash(const char* Key, int TablSize);
-
-struct ListNode;
-typedef struct ListNode* Position;
 struct HashTbl;
 typedef struct HashTbl* HashTable;
 
-HashTable InitializeTable(int TableSize);
-void DestroyTable(HashTable H);
+HashTable InitalizeTable(int TableSize);
+//void DestroyTable(HashTable H);
 Position Find(const char* Key, HashTable H);
 void Insert(const char* Key, HashTable H);
-int Retrieve(Position P);
-void Display(HashTable H);
+//const char* Retrieve(HashTable H);
+
+Index Hash(const char* Key, int TablSize);
