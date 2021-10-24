a1b823381cb2a9d13fd984dd19c692fad6e8f069
BoYang_Zhang <91548907+YangInChina@users.noreply.github.com>
2021-10-24 20:17:23 +0800

    Create _LeftHeap.c

---
+++ week4/ch02/_LeftHeap.c
@@ -0,0 +1,101 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef int ElementType;
+int MaxElements = 20 ;
+
+struct TreeNode{
+	ElementType Element;
+	PriorityQueue Left;
+	PriorityQueue Right;
+	int Npl;
+};
+
+PriorityQueue Initialize( void )
+{
+	return NULL;
+}
+
+PriorityQueue BuildHead( PriorityQueue H )
+{
+	return H = ( PriorityQueue )malloc( sizeof ( struct TreeNode ));
+}
+
+int IsEmpty( PriorityQueue H )
+{
+	return H == NULL;
+}
+
+void SwapChild(PriorityQueue H)
+{
+	PriorityQueue tmp = H->Left;
+	H->Left = H->Right;
+	H->Right = tmp;
+}
+
+PriorityQueue Merge( PriorityQueue H1, PriorityQueue H2 )
+{
+	if( H1 == NULL )
+		return H2;
+	if( H2 == NULL )
+		return H1;
+	if( H1->Element < H2->Element )
+		return Mergel( H1, H2);
+	else
+		return Mergel( H2, H1);
+}
+
+PriorityQueue Mergel( PriorityQueue H1, PriorityQueue H2 )
+{
+	if (H1->Left==NULL)
+	{
+		H1->Left = H2;
+	}
+	else
+	{
+		H1->Right = Merge(H1->Right,  H2);
+		if (H1->Left->Npl < H1->Right->Npl)
+		{
+			SwapChild(H1);
+		}
+		H1->Npl = H1->Right->Npl + 1;
+	} 
+	return H1;
+}
+
+PriorityQueue Insertl( ElementType X, PriorityQueue H )
+{
+	PriorityQueue SingleNode;
+	SingleNode = (PriorityQueue) malloc( sizeof( struct TreeNode ) );
+	if( SingleNode == NULL )
+		printf( "Out of space! ! ! " );
+	else
+	{
+		SingleNode->Element = X;
+		SingleNode->Npl = 0;
+		SingleNode->Left = SingleNode->Right = NULL;
+		H= Merge( SingleNode, H );
+	}
+	return H;
+}
+
+PriorityQueue DeleteMinl( PriorityQueue H )
+{
+	PriorityQueue LeftHeap, RightHeap;
+
+	if( IsEmpty( H ) )
+	{
+		printf( "Out of space! ! ! " );
+		return H;
+	}
+
+	LeftHeap = H->Left;
+	RightHeap = H->Right;
+	free(H);
+	return Merge( LeftHeap, RightHeap );
+}
+
+int main()
+{
+	return 0;
+}
